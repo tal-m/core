ac4c69ec6dfa5bfa4d016da08635d7804b03ce49
Erik <erik@montnemery.com>
2022-03-10 12:12:24 +0100

    Hide switch_as_x tracked entity

--- homeassistant/components/switch_as_x/__init__.py
+++ homeassistant/components/switch_as_x/__init__.py
@@ -23,7 +23,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up a config entry."""
     registry = er.async_get(hass)
     try:
-        er.async_validate_entity_id(registry, entry.options[CONF_ENTITY_ID])
+        entity_id = er.async_validate_entity_id(registry, entry.options[CONF_ENTITY_ID])
     except vol.Invalid:
         # The entity is identified by an unknown entity registry ID
         _LOGGER.error(
@@ -31,6 +31,13 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             entry.options[CONF_ENTITY_ID],
         )
         return False
+
+    # Hide the wrapped entry if registered
+    entity_entry = registry.async_get(entity_id)
+    if entity_entry is not None and not entity_entry.hidden:
+        registry.async_update_entity(
+            entity_id, hidden_by=er.RegistryEntryHider.INTEGRATION
+        )
 
     hass.config_entries.async_setup_platforms(entry, (entry.options["target_domain"],))
     return True
@@ -41,3 +48,20 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     return await hass.config_entries.async_unload_platforms(
         entry, (entry.options["target_domain"],)
     )
+
+
+async def async_remove_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """Unload a config entry."""
+    # Unhide the wrapped entry if registered
+    registry = er.async_get(hass)
+    try:
+        entity_id = er.async_validate_entity_id(registry, entry.options[CONF_ENTITY_ID])
+    except vol.Invalid:
+        # The source entity has been removed from the entity registry
+        return
+
+    if not (entity_entry := registry.async_get(entity_id)):
+        return
+
+    if entity_entry.hidden_by == er.RegistryEntryHider.INTEGRATION:
+        registry.async_update_entity(entity_id, hidden_by=None)
